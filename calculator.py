55df12f9a58366039ebe6e2797f435f6a06719a1
jslagle9 <jslagle@tesla.com>
2017-07-18 21:35:46 -0700

    Create calculator.py
    core file for landed cost calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print('What is the value of your shipment?')
+value = input()
+value = float(value)
+mpf = float()
+mpf = value * .003464
+if mpf >= 485
+  mpf = 485
+elif mpf <= 25
+  mpf = 25
+else mpf = value * .003464
+landed_cost = value + mpf
+print('The cost plus merchandise fee is $' +landed_cost)
